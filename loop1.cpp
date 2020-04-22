70ddfc337c277bc47ac76e62f240a3732e89b7b0
siemo13 <64165427+siemo13@users.noreply.github.com>
2020-04-22 22:39:54 +0200

    Create loop1.cpp

---
+++ loop1.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+# include <string>
+using namespace std;
+
+class wczytaj{
+  int a;
+  double d;
+  char c; 
+  public :
+  wczytaj();
+};
+  //wczytaj ::wczytaj(){
+  //  cout<<"jestem\n";
+  //}
+
+int main() {
+  string st(12,'q');
+  cout << "Hello World!\n";
+  for(int i=0;i<1000;i++)
+    cout<<st<<endl;
+  cout<<"koniec \n";
+}
